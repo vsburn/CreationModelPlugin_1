16481615d684b5b9f46e27a485cdab6b7a948c63
Василий <fcss.void@gmail.com>
2022-03-02 14:29:34 +0300

    Добавьте файлы проекта.

---
+++ CreationModelPlugin1.cs
@@ -0,0 +1,71 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlugin_1
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CreationModelPlugin1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            Level level1 = SelectLevel(doc, "Уровень 1");
+            Level level2 = SelectLevel(doc, "Уровень 2");
+
+            Transaction ts = new Transaction(doc, "Коробочка");
+            ts.Start();
+            CreateWalls(level1, level2, doc);
+            ts.Commit();
+
+            return Result.Succeeded;
+        }
+
+        public List<Wall> CreateWalls(Level level1, Level level2, Document doc)
+        {
+            double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
+            double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
+
+            double dx = width / 2;
+            double dy = depth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            List<Wall> walls = new List<Wall>();
+            for (int i = 0; i < 4; i++)
+            {
+                points.Add(new XYZ(-dx, -dy, 0));
+                points.Add(new XYZ(dx, -dy, 0));
+                points.Add(new XYZ(dx, dy, 0));
+                points.Add(new XYZ(-dx, dy, 0));
+                points.Add(new XYZ(-dx, -dy, 0));
+
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+
+                walls.Add(wall);
+            }
+            return walls;
+        }
+
+        public Level SelectLevel(Document doc, string lvlName)
+        {
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Level level = listLevel
+                .Where(x => x.Name.Equals(lvlName))
+                .FirstOrDefault();
+
+            return level;
+        }
+    }
+}
